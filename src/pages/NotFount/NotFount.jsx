272efc72150e23a39a23f0371c9027b23dad251f
luroalfa <rodriguezfallasluis@gmail.com>
2023-04-19 15:22:29 -0600

    Page NotFound is done

--- src/pages/NotFount/NotFount.jsx
+++ src/pages/NotFount/NotFount.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import Header from "../../components/Header/Header";
+import { Video } from "./Style/StyleElmentsNotFound";
 
 const NotFount = () => {
+  const UrlVdeo = "https://avacusvideofaqs.s3.amazonaws.com/notfound.mp4";
   return (
     <>
       <Helmet>
         <title>AV4CUS-Not Fount</title>
       </Helmet>
-      <h1>NotFount</h1>
+      <Header />
+      <Video src={UrlVdeo} controls autoPlay width={"100%"}>
+        <source src={UrlVdeo} type="video/mp4" />
+      </Video>
     </>
   );
 };
---
+++ src/pages/NotFount/Style/StyleElmentsNotFound.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { mediaQueries } from "../../../styles/responsive";
+
+
+export const Video = styled.video`
+
+${mediaQueries.md} {
+    height: 100vh;
+  }
+`;
