a1cd729622b648c45b71b14a138285b28da9ac2d
luroalfa <rodriguezfallasluis@gmail.com>
2023-04-21 11:15:49 -0600

    Information added about move0n

--- src/store/faqData.js
+++ src/store/faqData.js
@@ -12,24 +12,30 @@ const faqData = [
     },
     {
       id: 2,
+      question: "¿Qué es Move0n?",
+      answer: "Aplicación tecnológica para el transporte corporativo que mejora los canales de comunicación control y logística de la movilidad urbana.",
+      video: null,
+    },
+    {
+      id: 3,
       question: "¿Cómo puedo adquirir los servicios de Synapse?",
       answer: "El Cliente puede adquirir los servicios de Synapse de Av4cus mediante una suscripción mensual que se basa en el número de usuarios administrativos y técnicos activos, con un precio de $30 USD por usuario.",
       video: null,
     },
     {
-      id: 3,
+      id: 4,
       question: "¿Qué sucede si no cancelo la mensualidad correspondiente a tiempo?",
       answer: "Si el Cliente no cancela la mensualidad correspondiente dentro de los primeros quince días hábiles del siguiente mes, a partir del día 16 se incluirá un cobro moratorio de UNO POR CIENTO (1%) diario, que se calculará sobre el principal vencido y no pagado. La mora operará bajo el concepto de incumplimiento en el pago del principal, sin necesidad de protesta, requerimiento o intimación.",
       video: null,
     },
     {
-      id: 4,
+      id: 5,
       question: "¿Como se puede buscar una orden?",
       answer: "Para facilitar el proceso de buscar una orden de pedido, hemos creado un video tutorial que muestra paso a paso cómo realizarlo. Al seguir las instrucciones del video, podrás buscar tu orden de manera rápida y sencilla. Además, el video te ayudará a visualizar mejor el proceso y responder a posibles dudas ¡No dudes en revisarlo para obtener más detalles!",
       video: URLVideoBuscarOrden,
     },
     {
-      id: 5,
+      id: 6,
       question: "¿Como se puede editar una orden?",
       answer: "Para facilitar el proceso de editar una orden de pedido, hemos creado un video tutorial que muestra paso a paso cómo realizarlo. Al seguir las instrucciones del video, podrás editar tu orden de manera rápida y sencilla. Además, el video te ayudará a visualizar mejor el proceso y responder a posibles dudas ¡No dudes en revisarlo para obtener más detalles!",
       video: URLVideoEditarOrden,
